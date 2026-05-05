961ea1a6944f45295635eb74651fa5c45d4991ca
shimaazakaria1414-code <shimaazakaria1414@gmail.com>
2026-05-05 10:39:42 +0300

    Add team counting logic in 231A_Team.py
    Implement solution to count teams based on input criteria.

---
+++ 231A_ Team.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+ 
+n = int(input())
+count = 0
+ 
+for _ in range(n):
+    p, v, t = map(int, input().split())
+    if p + v + t >= 2:
+        count += 1
+ 
+print(count)
