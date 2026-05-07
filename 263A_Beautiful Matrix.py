c397eed03fa422f776fa0c79b43b5eb8cd22f9d0
shimaazakaria1414-code <shimaazakaria1414@gmail.com>
2026-05-07 18:15:06 +0300

    Implement distance calculation for '1' in matrix
    Calculate the distance of '1' from the center of a 5x5 matrix.

---
+++ 263A_Beautiful Matrix.py
@@ -0,0 +1,7 @@
+import sys
+input = sys.stdin.readline  
+matrix = [list(map(int, input().split())) for _ in range(5)]
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            print(abs(i-2)+abs(j-2))   
